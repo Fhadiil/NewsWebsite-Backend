8439b228d3c2e971b234b210e5a1aa0cb3ae19de
Fhadiil <fadeelaliyu51@gmail.com>
2025-01-05 14:10:05 +0100

    added postgressSQL

--- backend/settings.py
+++ backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,6 +88,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES['default'] = dj_database_url.parse('postgresql://admin:irFcFxRgA2oEjbUDAkGws80eA8CLhJvl@dpg-ctt8418gph6c738gkhb0-a.oregon-postgres.render.com/news_cws1')
 
 
 # Password validation

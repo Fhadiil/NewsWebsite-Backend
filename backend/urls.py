63776081911f9c5838ea71011d84192d5d1d524b
Fhadiil <fadeelaliyu51@gmail.com>
2025-01-05 14:35:25 +0100

    bugs fix

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.core.management import call_command
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('news.urls')),
+    path('create-superuser/', lambda request: (call_command('createsuperuser'), HttpResponse('Superuser created.'))),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
